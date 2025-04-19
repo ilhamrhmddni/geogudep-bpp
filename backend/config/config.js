c7a3e9e73d37341359ce2170c14a6b5cf7dbf3d2
ilhamrhmddni <ilhamrhmddni0@gmail.com>
2025-04-20 06:17:49 +0800

    be

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,18 +1,13 @@
 require("dotenv").config();
 
 module.exports = {
-  development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-  },
-  production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME_PROD,
-    host: process.env.DB_HOST,
-    dialect: "postgres",
+  databaseUrl: process.env.SUPABASE_API_URL,
+  dialect: "postgres",
+  dialectModule: require("pg"),
+  dialectOptions: {
+    ssl: {
+      require: false,
+      rejectUnauthorized: false, // Supabase butuh ini
+    },
   },
 };
--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,17 @@
-require("dotenv").config();
 const { Sequelize } = require("sequelize");
+require("dotenv").config();
+const config = require("./config");
 
-const sequelize = new Sequelize(process.env.DB_URL, {
-  dialect: "postgres",
-  dialectModule: require("pg"),
-  logging: false, // Matikan logging query
-  pool: {
-    max: 10, // Maksimum 10 koneksi
-    min: 1, // Minimal 1 koneksi standby
-    acquire: 30000, // Waktu tunggu 30 detik sebelum error
-    idle: 10000, // Koneksi idle ditutup setelah 10 detik
-  },
+const sequelize = new Sequelize(config.databaseUrl, {
+  dialect: config.dialect,
+  dialectOptions: config.dialectOptions,
+  logging: false,
 });
+
+// Cek koneksi database
+sequelize
+  .authenticate()
+  .then(() => console.log("✅ Database connected via Connection Pool!"))
+  .catch((err) => console.error("❌ Database connection error:", err));
 
 module.exports = sequelize;
