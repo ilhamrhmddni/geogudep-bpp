e5b2ee9e839e482de92c7406c0d56bc4da7d22d8
ilhamrhmddni <ilhamrhmddni0@gmail.com>
2025-04-21 01:31:01 +0800

    be

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,20 +2,30 @@ const { Sequelize } = require("sequelize");
 const config = require("./config");
 
 if (!config.databaseUrl) {
-  throw new Error("❌ DATABASE_URL is not defined in the configuration.");
+  console.error("❌ DATABASE_URL is not defined in the environment variables.");
+  process.exit(1);
 }
 
 const sequelize = new Sequelize(config.databaseUrl, {
   dialect: config.dialect,
+  dialectModule: config.dialectModule,
   dialectOptions: config.dialectOptions,
   logging: false, // Disable logging for production
   pool: config.pool,
 });
 
-// Check database connection
-sequelize
-  .authenticate()
-  .then(() => console.log("✅ Database connected successfully!"))
-  .catch((err) => console.error("❌ Database connection error:", err));
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log(
+      "✅ Connection to the database has been established successfully."
+    );
+  } catch (error) {
+    console.error("❌ Unable to connect to the database:", error.message);
+    process.exit(1);
+  }
+}
+
+testConnection();
 
 module.exports = sequelize;
